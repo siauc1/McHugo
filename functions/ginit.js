9b6917fcec40cd92e1e3efe3acbd45a368b9cf51
Evan <evanjo01@gmail.com>
2021-03-05 11:21:02 -0500

    Will probably regret the laziness later...

--- functions/ginit.js
+++ functions/ginit.js
@@ -4,7 +4,7 @@ const axios = require('axios').default;
 exports.handler = async (event, context, callback) => {
   try {
 
-    const doc = new GoogleSpreadsheet('1quYbWU9PCt0rWHqtTWVVw61MpFtMxJJKdbl5A-4G2ZM')
+    const doc = new GoogleSpreadsheet('1-DfIJh4JPrz_cp8ai-cHftwrOxwwa2G_Bwp9PDu9e2E')
     await doc.useServiceAccountAuth(require('./netlify.json'))
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[0];//const sheet = await doc.addSheet({ headerValues: ['t', 'ip'] });
